6b30a9183092e8a89ef3e7c12e888ba6f86a7980
bumjung <bumj94@gmail.com>
2015-06-09 23:08:35 -0700

    Add javascript to add 'me' beside text in GROUPS only

--- src/inject/js/inject.js
+++ src/inject/js/inject.js
@@ -1,13 +1,48 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+window.addEventListener('load', function() {
+	var checkJSInit = setInterval(function(){
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+		// HACK: poll until messages are rendered
+		if(document.querySelectorAll('._1t_p').length > 0) {
+			clearInterval(checkJSInit);
+			
+			renderMyName();
 
-	}
-	}, 10);
+			var container = document.querySelector('._4bl9 ._4u-c._1wfr .uiScrollableAreaWrap');
+			container.addEventListener('scroll', function() {
+				
+				// add new elements when new messages are loaded
+				if(container.scrollTop === 0) {
+					var messagesCount = document.querySelectorAll('._1t_p').length;
+					
+					// HACK: poll until new messages are rendered
+					var pollUntilMessageLoaded = setInterval(function(){
+						if(messagesCount !== document.querySelectorAll('._1t_p').length){
+							renderMyName();
+						}
+
+					}, 100);
+				}
+			});
+		}
+			}, 100);
 });
+
+function renderMyName() {
+	var messageBoxes = document.querySelectorAll('._1t_p');
+	for(var i=0; i < messageBoxes.length; i++){
+		var children = messageBoxes[i].children;
+
+		// it's only one if the message container <li> is by me
+		if(children.length === 1) {
+			var myNameDiv = document.createElement('div');
+			myNameDiv.className = "_ih3 myName";
+			messageBoxes[i].appendChild(myNameDiv);
+			messageBoxes[i].insertBefore(myNameDiv, messageBoxes[i].firstChild);
+
+			var myNameSpan = document.createElement('span');
+			myNameSpan.className = "_3oh-";
+			myNameSpan.innerHTML = "Me";
+			myNameDiv.appendChild(myNameSpan);
+		}
+	}
+}
